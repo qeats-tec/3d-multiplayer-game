ba85ee73d0fff986f3016845542461b641c56e97
Qeat <ahmediarda.02@gmail.com>
2026-01-08 19:34:36 +0300

    Refactor socket.io connection handling and player management

--- server/server.js
+++ server/server.js
@@ -4,37 +4,58 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+  },
 });
 
-let players = {};
-let names = {};
+const players = {};
 
-io.on("connection", socket => {
-  console.log("Oyuncu bağlandı:", socket.id);
+io.on("connection", (socket) => {
+  console.log("Bir oyuncu bağlandı:", socket.id);
 
-  socket.emit("me", socket.id);
+  socket.on("join", (username) => {
+    players[socket.id] = {
+      id: socket.id,
+      username,
+      x: 0,
+      y: 1,
+      z: 0,
+    };
 
-  players[socket.id] = { x: 0, z: 0 };
-
-  socket.on("setName", name => {
-    names[socket.id] = name;
+    socket.emit("currentPlayers", players);
+    socket.broadcast.emit("newPlayer", players[socket.id]);
   });
 
-  socket.on("move", pos => {
-    players[socket.id] = pos;
-    io.emit("players", { players, names });
+  socket.on("move", (pos) => {
+    if (players[socket.id]) {
+      players[socket.id] = { ...players[socket.id], ...pos };
+      socket.broadcast.emit("playerMoved", players[socket.id]);
+    }
+  });
+
+  socket.on("chat", (msg) => {
+    if (players[socket.id]) {
+      io.emit("chat", {
+        user: players[socket.id].username,
+        message: msg,
+      });
+    }
   });
 
   socket.on("disconnect", () => {
     delete players[socket.id];
-    delete names[socket.id];
-    io.emit("players", { players, names });
+    io.emit("playerDisconnected", socket.id);
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server çalışıyor");
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("Server çalışıyor");
+  console.log("SERVER BAŞLADI:", PORT);
 });
